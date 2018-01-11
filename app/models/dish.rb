b5f1e1ccc30b82703aeb380a53384571c467aa25
Raghu Betina <raghu@firstdraft.com>
2018-01-11 15:48:49 +0000

    Direct association between dishes and filters

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,9 @@
 class Dish < ApplicationRecord
   # Direct associations
 
+  has_many   :filters,
+             :dependent => :destroy
+
   has_many   :favorites,
              :foreign_key => "dishes_id",
              :dependent => :destroy
--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,6 +1,8 @@
 class Filter < ApplicationRecord
   # Direct associations
 
+  belongs_to :dish
+
   # Indirect associations
 
   # Validations
