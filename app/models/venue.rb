30335cac3b6b767370a75f5441d88671555582ae
Raghu Betina <raghu@firstdraft.com>
2018-01-11 15:48:49 +0000

    Direct association between venues and favorites

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,10 @@
 class Venue < ApplicationRecord
   # Direct associations
 
+  has_many   :favorites,
+             :foreign_key => "venues_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
